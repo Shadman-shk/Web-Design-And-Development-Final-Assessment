a2ed309077c98c5e0eabae7e66264720f29de121
Shadman <hossainshadman005@gmail.com>
2026-01-20 01:55:38 +1100

    Created Database class and Tables
    -Created the database class for storing the information
    
    - Created 4 classes which are User, TourPackages, Booking and Reviews

---
+++ Tourism Website/Data/TourismContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+
+namespace Tourism_Website.Data
+{
+    public class TourismContext : DbContext
+    {
+        public TourismContext() : base("TourismDB") { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<TourPackage> TourPackages { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+    }
+}
